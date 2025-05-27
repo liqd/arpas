521600fcdce5b61e74f696a8c647bef237e332ae
Philipp Veller <mail@vellip.de>
2025-05-27 12:29:26 +0200

    ar: pass minio secrets from django to the ARC

--- apps/augmentedreality/templatetags/react_augmentedreality.py
+++ apps/augmentedreality/templatetags/react_augmentedreality.py
@@ -1,6 +1,7 @@
 import json
 
 from django import template
+from django.conf import settings
 from django.utils.html import format_html
 
 from apps.augmentedreality.serializers import SceneSerializer
@@ -18,6 +19,9 @@ def react_augmentedreality_arc(topic):
 
     if topic.scene:
         attributes["scene"] = SceneSerializer(topic.scene).data
+
+    if hasattr(settings, "MINIO_DATA"):
+        attributes["minioData"] = settings.MINIO_DATA
 
     return format_html(
         '<div data-arpas-widget="arc" ' 'data-attributes="{attributes}"></div>',
